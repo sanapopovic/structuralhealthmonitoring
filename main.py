910fb32ed13b8ab1f27ada5001a55d3979a42949
sanapopovic <sanapopovic77@gmail.com>
2026-03-12 10:29:33 +0100

    wavelet functions

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import scipy as sp
 import preprocess
 import transforms.stft_processing as stft_processing
 import matplotlib.pyplot as plt
+from transforms.wavelet_processing import wavelet_decompose
+from transforms.wavelet_processing import wavelet_scalogram
 
 #All files should be uploaded as csv
 data = preprocess.get_data(r"Data/In-plane_A2_TemporalResponse@15.963MHzmm@200mm.csv")
@@ -15,3 +17,16 @@ preprocess.plot(t, y, 10, 'time_vs_volt')
 f, t_seg, amplitude, fs = stft_processing.stft(y, t)
 
 stft_processing.plot_stft(f, t_seg, amplitude, downsampling=1, name="Spectrogram")
+
+coeffs = wavelet_decompose(y, wavelet='db4', level=None)
+
+# grab first detail level (finest scale)
+cD1 = coeffs[-1]
+
+# simple time axis to match its length
+import numpy as np
+t_cD1 = np.linspace(t.iloc[0], t.iloc[-1], len(cD1))
+
+preprocess.plot(t_cD1, cD1, 1, 'wavelet_detail_L1')
+
+wavelet_scalogram(t, y, wavelet='cmor1.5-1.0', n_scales=100, name="wavelet_scalogram")
