ecb98002ce1e26035cb794adcce51790499162d5
sanapopovic <sanapopovic77@gmail.com>
2026-03-12 09:11:12 +0100

    data

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import functions as func
 import matplotlib.pyplot as plt
 
 #All files should be uploaded as csv
-data = func.get_data(r"Data\In-plane_A2_TemporalResponse@15.963MHzmm@200mm.csv")
+data = func.get_data(r"Data/In-plane_A2_TemporalResponse@15.963MHzmm@200mm.csv")
 
 t = data["Propagation time (micsec)"]
 y = data["Sum Propagated signal (nm)"]
