3253581153c7465911022738d425d0e3dbcae45f
Jesse Misdorp <j.misdorp@student.tudelft.nl>
2026-05-07 14:40:38 +0200

    attempt 1 VMD

--- VMD.py
+++ VMD.py
@@ -1,6 +1,19 @@
 import numpy as np
 import pandas as pd
 from preprocess import get_data
+import matplotlib.pyplot as plt  
+from vmdpy import VMD
 
 
 data = get_data(r"Data\In-plane_A2_TemporalResponse@15.963MHzmm@200mm.xlsx")
+
+#. some sample parameters for VMD  
+alpha = 2000       # moderate bandwidth constraint  
+tau = 0.            # noise-tolerance (no strict fidelity enforcement)  
+K = 3              # 3 modes  
+DC = 0             # no DC part imposed  
+init = 1           # initialize omegas uniformly  
+tol = 1e-7 
+
+#. Run actual VMD code  
+u, u_hat, omega = VMD(data, alpha, tau, K, DC, init, tol) 
